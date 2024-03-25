c40c50298c4eaae7cf1a07df59e8d2b77b6044aa
manish4256 <41384648+manish4256@users.noreply.github.com>
2024-03-25 13:06:40 +0530

    Update Test_gray.py

--- Synthetic/Test_gray.py
+++ Synthetic/Test_gray.py
@@ -45,7 +45,7 @@ def my_Vfilter(shape, dtype=None):
 #ParsingArguments
 parser=argparse.ArgumentParser()
 parser.add_argument('--dataPath',dest='dataPath',type=str,default='./Testing_data/BSD68/',help='testDataPath')
-parser.add_argument('--weightsPath',dest='weightsPath',type=str,default='./Pretrained_models/final_new.h5',help='pathOfTrainedCNN')
+parser.add_argument('--weightsPath',dest='weightsPath',type=str,default='./Pretrained_models/itsnew.h5',help='pathOfTrainedCNN')
 args=parser.parse_args()
 #createModel, loadWeights
 def custom_loss(y_true,y_pred): #this is required for loading a keras-model created with custom-loss
@@ -65,22 +65,22 @@ for path in listPaths:
     (cv2.imread(str(path),0))))
 imgTestArray=np.array(imgTestArray)/255.
 
-noise_level_img = 25 #noise level for noisy image
-lenth =68
+noise_level_img = 30 #noise level for noisy image
+length =68
 sumPSNR=0
 sumSSIM=0
-psnr_val=np.empty(lenth)
-ssim_val=np.empty(lenth)
-for i in range(0,lenth):
+psnr_val=np.empty(length)
+ssim_val=np.empty(length)
+for i in range(0,length):
     np.random.seed(seed=0)  # for reproducibility
     img1=imgTestArray[i]
     f=img1 + np.random.normal(0, noise_level_img/255., img1.shape)
     error=nmodel_PROPOSED.predict(np.expand_dims(f,axis=0))
     predClean=f-np.squeeze(error)
     z=(predClean)
-    cv2.imwrite("./25new/"+str(i+1)+"_Original.png",255.*img1)
-    cv2.imwrite("./25new/"+str(i+1)+"_Proposed.png",255.*z)
-    cv2.imwrite("./25new/"+str(i+1)+"_Noisy.png",255.*f)
+    cv2.imwrite("./result/"+str(i+1)+"_Original.png",255.*img1)
+    cv2.imwrite("./result/"+str(i+1)+"_Proposed.png",255.*z)
+    cv2.imwrite("./result/"+str(i+1)+"_Noisy.png",255.*f)
     psnr_val[i]=util.calculate_psnr(255.*z,255.*img1)
     ssim_val[i]=util.calculate_ssim(255.*z,255.*img1)
     print('PSNR of image '+str(i+1)+' is ',psnr_val[i])
