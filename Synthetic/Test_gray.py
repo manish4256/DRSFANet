9bbda30e3e505f01fc46491601dee55bfb214c59
manish4256 <41384648+manish4256@users.noreply.github.com>
2024-04-27 14:54:10 +0530

    Update Test_gray.py

--- Synthetic/Test_gray.py
+++ Synthetic/Test_gray.py
@@ -45,7 +45,7 @@ def my_Vfilter(shape, dtype=None):
 #ParsingArguments
 parser=argparse.ArgumentParser()
 parser.add_argument('--dataPath',dest='dataPath',type=str,default='./Testing_data/BSD68/',help='testDataPath')
-parser.add_argument('--weightsPath',dest='weightsPath',type=str,default='./Pretrained_models/DRSFANet_Gray.h5.h5',help='pathOfTrainedCNN')
+parser.add_argument('--weightsPath',dest='weightsPath',type=str,default='./Pretrained_models/DRSFANET_Gray.h5.h5',help='pathOfTrainedCNN')
 args=parser.parse_args()
 #createModel, loadWeights
 def custom_loss(y_true,y_pred): #this is required for loading a keras-model created with custom-loss
